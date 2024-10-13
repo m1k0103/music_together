bdd19f274914ba14a4b3436bd4becae7765d2046
mikok <84585119+miko1034@users.noreply.github.com>
2024-10-13 19:08:09 +0100

    get_db_name, make func

---
+++ music_together/func.py
@@ -0,0 +1,46 @@
+import sqlite3
+import yaml
+
+def get_db_name():
+    try:
+        with open("config.yaml", "r") as cfg:
+            contents = yaml.safe_load(cfg)
+            name = contents["database_name"]
+            return name
+    except:
+        print("[!] NO CONFIG FILE FOUND")
+        quit()
+
+class Database:
+    def __init__(self,name):
+        self.name = name
+    
+    def make(self):
+        con = sqlite3.connect(self.name)
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE songs(
+                    sid int PRIMARY KEY,
+                    source TEXT,
+                    music_path TEXT
+                    )""")
+        cur.execute("""CREATE TABLE queues(
+                    qid INT PRIMARY KEY,
+                    count INT,
+                    song_id INT,
+                    FOREIGN KEY(song_id) REFERENCES songs(sid)
+                    )""")
+        cur.execute("""CREATE TABLE users(
+                    uid INT PRIMARY KEY,
+                    username TEXT,
+                    phash TEXT
+                    )""")
+        cur.execute("""CREATE TABLE rooms(
+                    rid INT PRIMARY KEY,
+                    capacity INT,
+                    queue INT,
+                    room_owner INT,
+                    FOREIGN KEY (queue) REFERENCES queue(qid),
+                    FOREIGN KEY (room_owner) REFERENCES users(uid)
+                    )""")
+        con.commit()
+        con.close()
