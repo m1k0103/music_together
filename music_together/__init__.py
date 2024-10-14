a14696a875d133d77538437c9ef5d235c2478bfe
mikok <84585119+miko1034@users.noreply.github.com>
2024-10-14 20:09:18 +0100

    secrets to config

--- music_together/__init__.py
+++ music_together/__init__.py
@@ -1,11 +1,16 @@
 import sqlite3
 from music_together.func import Database, get_db_name
 import os
+import secrets
 
 def start():
     db = Database(get_db_name())
     db.make()
     print("[+] Database created successfully.")
+
+    if "config.yaml" not in os.listdir():
+        with open("config.yaml", "w+") as f:
+            f.write(f"""database_name: "database.db" # default: database.db\nsecret: "{secrets.token_hex(20)}" """)
 
     from music_together.routes import app
     app.run(host="0.0.0.0", port="5000", debug=True)
