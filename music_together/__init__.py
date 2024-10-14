e849cc3fc9e90d45c2d19b1be348a6bd94ed4625
mikok <84585119+miko1034@users.noreply.github.com>
2024-10-14 21:29:09 +0100

    idk what i changed

--- music_together/__init__.py
+++ music_together/__init__.py
@@ -9,8 +9,11 @@ def start():
     print("[+] Database created successfully.")
 
     if "config.yaml" not in os.listdir():
-        with open("config.yaml", "w+") as f:
-            f.write(f"""database_name: "database.db" # default: database.db\nsecret: "{secrets.token_hex(20)}" """)
+        f = open("config.yaml", "w+")
+        f.write(f"""database_name: "database.db"\nsecret: "{secrets.token_hex(20)}" """)
+        f.close()
+    else:
+        pass
 
     from music_together.routes import app
     app.run(host="0.0.0.0", port="5000", debug=True)
