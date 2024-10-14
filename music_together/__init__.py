0a7a8087db2828002173e1478d3b90cbda3a36ec
miki <84585119+miko1034@users.noreply.github.com>
2024-10-14 18:30:13 +0100

    init now stars app

--- music_together/__init__.py
+++ music_together/__init__.py
@@ -1,7 +1,11 @@
 import sqlite3
 from music_together.func import Database, get_db_name
+import os
 
 def start():
     db = Database(get_db_name())
     db.make()
     print("[+] Database created successfully.")
+
+    from music_together.routes import app
+    app.run(host="0.0.0.0", port="5000", debug=True)
